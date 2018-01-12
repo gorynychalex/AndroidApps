bd17bad3eb9ed5f1e283cde05b186825e4ba9718
gorynychalex <gorynychalex@gmail.com>
2018-01-13 09:13:21 +1000

    listView modify

--- app07sqlite/src/main/java/com/example/myapplication/DBHelper.java
+++ app07sqlite/src/main/java/com/example/myapplication/DBHelper.java
@@ -1,5 +1,6 @@
 package com.example.myapplication;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
@@ -10,12 +11,33 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 public class DBHelper extends SQLiteOpenHelper {
 
-    private static final String DB_NAME = "mydatabase";
-    private static final int DB_VERSION = 1;
-    private static final String TABLE_MAIN = "questions";
-    private static final String TABLE_MAIN_ID = "id";
-    private static final String TABLE_MAIN_TEXT = "question_text";
-    private static final String TABLE_MAIN_OPTIONS = "questions_options";
+    public static final String TAG = "DBHelper";
+
+    // Переменные базы данных
+    public static final String DB_NAME = "mydatabase";
+    public static final int DB_VERSION = 5;
+
+    // Таблица тесты и ее поля: [quiz] { id, text }
+    public static final String TABLE_QUIZ = "quiz";
+    public static final String TABLE_QUIZ_ID = "_id";
+    public static final String TABLE_QUIZ_NAME = "question_text";
+
+    // Таблица вопросы и ее поля: [questions] { id, quiz_id, text }
+    public static final String TABLE_QUESTIONS = "questions";
+    public static final String TABLE_QUESTION_ID = "_id";
+    public static final String TABLE_QUESTION_QUIZ_ID = "quiz_id";
+    public static final String TABLE_QUESTION_TEXT = "question_text";
+
+    // Таблица вариантов ответов и поля: [options] { id, quiestion_id, text, correct }
+    public static final String TABLE_OPTIONS = "options";
+    public static final String TABLE_OPTION_ID = "_id";
+    public static final String TABLE_OPTION_QUESTION_ID = "question_id";
+    public static final String TABLE_OPTION_TEXT = "option_text";
+    public static final String TABLE_OPTION_CORRECT = "option_correct";
+
+
+    // Массив для первичного заполнения тестов
+    String[] tests = {"Java", "Android", "Patterns", "Gradle"};
 
     public DBHelper(Context context) {
         super(context, DB_NAME, null, DB_VERSION);
@@ -23,16 +45,50 @@ public class DBHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
-        sqLiteDatabase.execSQL("create table " + TABLE_MAIN + "("
-                + TABLE_MAIN_ID + " integer primary key, "
-                + TABLE_MAIN_TEXT + " text, "
-                + TABLE_MAIN_OPTIONS + " text " + ")"
+
+        // Создание таблицы тестов
+        sqLiteDatabase.execSQL("create table " + TABLE_QUIZ + "("
+                + TABLE_QUIZ_ID + " integer primary key, "
+                + TABLE_QUIZ_NAME + " text"
+                + ")"
         );
-//        "create table questions (id integer primary key, question_text text, question_options text"
+
+        // Создание таблицы вопросов. Внешний ключ указывает на таблицу тестов
+        sqLiteDatabase.execSQL("create table " + TABLE_QUESTIONS + "("
+                + TABLE_QUESTION_ID + " integer primary key, "
+                + TABLE_QUESTION_TEXT + " text, "
+                + TABLE_QUESTION_QUIZ_ID + " text, "
+                + " FOREIGN KEY(" + TABLE_QUESTION_QUIZ_ID + ") "
+                + " REFERENCES " + TABLE_QUIZ + "(" + TABLE_QUIZ_ID + ")"
+                + ")"
+        );
+
+        // Создание таблицы вариантов ответов. Внешний ключ указывает на таблицу вопросов.
+        sqLiteDatabase.execSQL("create table " + TABLE_OPTIONS + "("
+                + TABLE_OPTION_ID + " integer primary key, "
+                + TABLE_OPTION_TEXT + " text, "
+                + TABLE_OPTION_CORRECT + " integer, "
+                + TABLE_OPTION_QUESTION_ID + " text, "
+                + " FOREIGN KEY(" + TABLE_OPTION_QUESTION_ID + ") "
+                + " REFERENCES " + TABLE_QUESTIONS + "(" + TABLE_QUESTION_ID + ")"
+                + ")"
+        );
+
+        // Первичное заполнение таблицы тестов
+        ContentValues cv = new ContentValues();
+        for (int i = 0; i < tests.length; i++) {
+            cv.put(TABLE_QUIZ_NAME, tests[i]);
+            sqLiteDatabase.insert(TABLE_QUIZ, null, cv);
+        }
     }
 
     @Override
-    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int oldVersion, int newVersion) {
 
+        sqLiteDatabase.execSQL("drop table if exists " + TABLE_QUIZ);
+        sqLiteDatabase.execSQL("drop table if exists " + TABLE_QUESTIONS);
+        sqLiteDatabase.execSQL("drop table if exists " + TABLE_OPTIONS);
+
+        onCreate(sqLiteDatabase);
     }
 }
--- app07sqlite/src/main/java/com/example/myapplication/MainActivity.java
+++ app07sqlite/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,13 +1,85 @@
 package com.example.myapplication;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Метка для журналирования
+    public static final String TAG = "MainActivity";
+
+    // Объявление переменной для списка
+    ListView listView;
+
+    // Переменная для кнопки
+    Button button;
+
+    // Переменная для инициализации DB
+    DBHelper dbHelper;
+
+    // Переменная для управления DB, через методы:
+    // query(),insert(),delete(),update(), execSQL()
+    SQLiteDatabase sqLiteDatabase;
+
+    // Переменная для курсора - временного объекта для хранения записей
+    Cursor cursor;
+
+    // Переменная для адаптера
+    SimpleCursorAdapter simpleCursorAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Определение списка из макета activity_main.xml
+        listView = (ListView) findViewById(R.id.list_view);
+
+        //Экземпляр класса базы данных
+        dbHelper = new DBHelper(this);
+        //объект класса для получения доступа к управлению с поддержкой записи данных
+        sqLiteDatabase = dbHelper.getWritableDatabase();
+
+        Log.d(TAG, "dbHelper.getWritableDatabase() has done");
+
+        // Объект Cursor типа MAP-коллекция
+        cursor = sqLiteDatabase.query(DBHelper.TABLE_QUIZ,null,null,null,null,null,null);
+
+        if(cursor.moveToFirst()) {
+            do {
+                // Сохранение данных из БД в отдельный массив
+//                names.add(cursor.getString(1) + " " + cursor.getString(2));
+                // Сохранение индексов "_id" для запросов в базу
+//                idNames.add(cursor.getInt(0));
+                Log.d(TAG, "Cursor = " + cursor.getPosition() + ", ID = " + cursor.getInt(0) + " , " + DBHelper.TABLE_QUIZ_NAME + " = " + cursor.getString(1));
+            } while (cursor.moveToNext());
+        }
+//        cursor.close();
+
+        simpleCursorAdapter = new SimpleCursorAdapter(this,
+                R.layout.list_item, cursor,
+                new String[]{DBHelper.TABLE_QUIZ_NAME}, new int[]{R.id.item},0);
+
+        listView.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
+        listView.setAdapter(simpleCursorAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MainActivity.this, "position = " + position, Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
     }
 }
