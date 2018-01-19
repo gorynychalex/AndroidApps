39228f02b6cdac1b943709c867ab9eb1aa233d5f
gorynychalex <gorynychalex@gmail.com>
2018-01-19 15:45:04 +1000

    app09 savefiles

---
+++ app09filesave/src/main/java/ru/popovich/app09filesave/MainActivity.java
@@ -0,0 +1,74 @@
+package ru.popovich.app09filesave;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * File save
+ */
+
+public class MainActivity extends AppCompatActivity {
+
+    String filename = "fileMainActivity";
+    FileOutputStream outputStream;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Log.d("MainActivity","context.getFileDir(): " + this.getFilesDir());
+
+        File file = new File(this.getFilesDir(), filename);
+
+        Log.d("MainActivity","getAbsolutefile(): " + file.getAbsoluteFile());
+
+        try {
+            outputStream = openFileOutput(filename, Context.MODE_PRIVATE);
+            PrintWriter printWriter = new PrintWriter(outputStream);
+            printWriter.println("My first line");
+            printWriter.println("Second line");
+            printWriter.close();
+
+//            outputStream.write("My text first line".getBytes());
+//            outputStream.write("Second line".getBytes());
+
+            outputStream.close();
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+
+
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
+
+            String line;
+            StringBuilder builder = new StringBuilder();
+            while ((line=bufferedReader.readLine())!=null){
+                builder.append(line);
+                Log.d("MainActivity","Read From File: " + line);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            Log.d("MainActivity", "FileNotFoundException");
+        } catch (IOException e) {
+            e.printStackTrace();
+            Log.d("MainActivity", "IOException");
+        }
+
+
+    }
+}
