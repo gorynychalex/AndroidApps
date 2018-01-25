da1bcf15783d3b58b6cb17bdf066c6518bc5b7ba
gorynychalex <gorynychalex@gmail.com>
2018-01-25 20:14:44 +1000

    app09 savefiles

--- app09filesave/src/main/java/ru/popovich/app09filesave/MainActivity.java
+++ app09filesave/src/main/java/ru/popovich/app09filesave/MainActivity.java
@@ -1,6 +1,7 @@
 package ru.popovich.app09filesave;
 
 import android.content.Context;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.JsonReader;
@@ -9,10 +10,12 @@ import android.util.Log;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 
@@ -28,57 +31,84 @@ import java.io.PrintWriter;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final String TAG = "MainActivity";
+
     //ОБЪЯВЛЕНИЕ И ИНИЦИАЛИЗАЦИЯ ИМЕНИ ФАЙЛА
-    public static final String FILENAME = "file1";
+    public static final String FILENAME = "file.txt";
 
     File file;
     FileOutputStream outputStream;
+    BufferedWriter bufferedWriter;
+    FileWriter fileWriter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Log.d("MainActivity","context.getFileDir(): " + this.getFilesDir());
+        Log.d(TAG,"context.getFileDir(): " + this.getFilesDir());
+
+        Log.d(TAG,"Environment.getExternalStorageDirectory(): " + Environment.getExternalStorageDirectory());
+        Log.d(TAG,"Environment.getExternalStorageState(): " + Environment.getExternalStorageState());
+        Log.d(TAG,"Environment.DIRECTORY_DOWNLOADS: " + Environment.DIRECTORY_DOCUMENTS);
+
+        // Проверка наличия стандартной директории
+        File dir = new File(Environment.getExternalStorageDirectory(), Environment.DIRECTORY_DOCUMENTS);
+        Log.d(TAG,"Environment.DIRECTORY_DOCUMENTS is Exist: " + dir.exists());
+        if(!dir.exists()) {
+            dir.mkdir();
+            Log.d(TAG, "Environment.DIRECTORY_DOCUMENTS is Exist: " + dir.exists());
+        }
 
         // Создание объекта файла
-        file = new File(this.getFilesDir(), FILENAME);
+        file = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS.toLowerCase()), FILENAME);
 
         if(file.exists()) {
-            Log.d("MainActivity", "FILE EXIST! getAbsolutefile(): " + file.getAbsoluteFile());
-
+            Log.d(TAG, "FILE EXIST! getAbsolutefile(): " + String.valueOf(file.getAbsoluteFile()));
         }
         else {
-            Log.d("MainActivity", "FILE IS NOT EXIST! getAbsolutefile(): " + file.getAbsoluteFile());
+            Log.d(TAG, "FILE IS NOT EXIST! getAbsolutefile(): " + String.valueOf(file.getAbsoluteFile()));
         }
 
         // ЗАПИСЬ ДАННЫХ
         try {
             // Создание потока вывода
-            outputStream = openFileOutput(FILENAME, Context.MODE_APPEND);
+            bufferedWriter = new BufferedWriter(new FileWriter(file,true));
+//            outputStream = openFileOutput(String.valueOf(file.getAbsoluteFile()), Context.MODE_APPEND);
+//            fileWriter = new FileWriter(file.getAbsoluteFile());
 
             // Инициализация форматированного буферризованного объекта записи
-            PrintWriter printWriter = new PrintWriter(outputStream);
+//            PrintWriter printWriter = new PrintWriter(outputStream);
 
-            // Создание JSON объекта
+            // Создание JSON объекта и добавление данных
             JSONObject jsonObject = new JSONObject();
+            jsonObject.put("first1","First field1");
+            jsonObject.put("second1","Second field1");
 
-            // Добавление данных в виде JSON
-            jsonObject.put("first","First field");
-            jsonObject.put("second","Second field");
-
-            // Запись данных в поток и его закрытие
+            // Запись данных в поток
+            bufferedWriter.write(jsonObject.toString());
 //            printWriter.write(jsonObject.toString());
-//            printWriter.close();
+//            outputStream.write(jsonObject.toString().getBytes());
+//            fileWriter.write(jsonObject.toString());
 
-            // Запись данных в поток и его закрытие
-            outputStream.write(jsonObject.toString().getBytes());
+//            fileWriter.flush();
 
-            // Закрытие потока вывода
-            outputStream.close();
+            Log.d(TAG, "File has just write!");
 
+        } catch (FileNotFoundException e){
+            Log.d(TAG, "FileNotFoundException");
         } catch (Exception e){
             e.printStackTrace();
+            Log.d(TAG, "Exception from file save");
+        } finally {
+            // Закрытие потока вывода
+            try {
+                bufferedWriter.close();
+//                fileWriter.close();
+//                outputStream.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
 
 
@@ -96,15 +126,15 @@ public class MainActivity extends AppCompatActivity {
 
             // Построчное чтение из файла
             while (jsonReader.hasNext()){
-                Log.d("MainActivity",jsonReader.nextName() + " : " + jsonReader.nextString());
+                Log.d(TAG,"Read:" + jsonReader.nextName() + " : " + jsonReader.nextString());
             };
 
         } catch (FileNotFoundException e) {
             e.printStackTrace();
-            Log.d("MainActivity", "FileNotFoundException");
+            Log.d(TAG, "FileNotFoundException");
         } catch (IOException e) {
             e.printStackTrace();
-            Log.d("MainActivity", "IOException");
+            Log.d(TAG, "IOException");
         } finally {
             try {
                 jsonReader.close();
